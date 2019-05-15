6cbf29d32e7bc9d22709123e327d5fd7c2bcf02b
Mia Szarvas <mia.szarvas@gmail.com>
2019-05-15 16:25:09 +0200

    build stupid yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+puts "Creating 5 new restaurants"
+
+restaurants_fake = []
+categories_array = ["chinese", "italian", "japanese", "french", "belgian"]
+5.times do
+  restaurants_fake << Restaurant.new(
+    {
+      name: Faker::Restaurant.name,
+      address: Faker::Address.full_address,
+      phone_number: Faker::PhoneNumber.phone_number,
+      category: categories_array.sample
+    }
+  ).save!
+end
